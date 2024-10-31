e34da53478180deea7a23a2053d6816f9c467694
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-10-31 13:01:04 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+// This brings in the iostream library
 #include <iostream>
 
+/*
+This is block comment
+*/
 int main()
 {
+    // This is going to print "Vazha" to the console
     std::cout << "Vazha";
     std::cout << " ";
     std::cout << "Chkimokocha";
     return 0;
+    // Program ends here
+
+   
 }
