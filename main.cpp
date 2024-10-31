33303a3f09259e36080687f8d345dfe8e54bdbd1
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-10-31 13:20:28 +0200

    errors

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 
 int main()
 {
-    std::cout << "Vazha";
-    std::cout << " ";
-    std::cout << "Chkimokocha";
+    // Compile time error
+    std::cout << "Hello World" << std::endl;
+
+    // Run time error
+    // int value = 7/0;
+    // std::cout << "value : " << value << std::endl;
+    
     return 0;
 }
