e3157131cf893855bbaad52d82e4c84cf9e83f55
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-11-01 20:57:43 +0200

    statements and functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 
+int addNumbers(int first_param, int second_param)
+{
+    int result = first_param + second_param;
+    return result;
+}
+
+int multiplyNumbers(int first_param, int second_param)
+{
+    int result = first_param * second_param;
+    return result;
+}
+
 int main()
 {
-    std::cout << "Vazha";
-    std::cout << " ";
-    std::cout << "Chkimokocha";
+    int first_number{13}; // Statement
+    int second_number{7};
+
+    std::cout << "First number: " << first_number << std::endl;   // 13
+    std::cout << "Second number: " << second_number << std::endl; // 7
+
+    int sum = first_number + second_number;
+    std::cout << "summary : " << sum << std::endl; // 20
+
+    sum = addNumbers(25, 7);
+    std::cout << "summary : " << sum << std::endl; // 32
+
+    sum = addNumbers(0, 10);
+    std::cout << "summary : " << sum << std::endl; // 10
+
+    std::cout << "sum : " << addNumbers(120, 130) << std::endl; // 250
+
+    std::cout << "multiply : " << multiplyNumbers(7, 8) << std::endl; // 56
+
     return 0;
 }
