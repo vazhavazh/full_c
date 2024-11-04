424529f5ad9e848af4206123041a7622de244d44
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-11-04 20:11:48 +0200

    numeric system

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 int main()
 {
-    std::cout << "Vazha";
-    std::cout << " ";
-    std::cout << "Chkimokocha";
+
+    // ! Representation of 15 
+    int number1 = 15;         // Decimal
+    int number2 = 017;        // Octal
+    int number3 = 0x0F;       // Hexadecimal
+    int number4 = 0b00001111; // Binary
+
+    std::cout << "number1: " << number1 << std::endl;
+    std::cout << "number2: " << number2 << std::endl;
+    std::cout << "number3: " << number3 << std::endl;
+    std::cout << "number4: " << number4 << std::endl;
+
     return 0;
 }
